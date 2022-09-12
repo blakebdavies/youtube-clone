fc2e1a1124b90b51bef5d4a10e793510c2844324
blakebdavies <blakebdavies@gmail.com>
2022-09-12 21:14:45 +0100

    Cleaned up file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import {BrowswerRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
-import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
-import SearchFeed from "./components/SearchFeed";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
 
-
-
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
 
 const App = () => (
-    <BrowswerRouter>
+    <BrowserRouter>
         <Box sx={{backgroundColor:'#000'}}>
             <Navbar />
             <Routes>
@@ -20,7 +14,7 @@ const App = () => (
                 <Route path="/search/:searchTerm" element={<SearchFeed />}/>
             </Routes>
         </Box>
-    </BrowswerRouter>
+    </BrowserRouter>
     );
 
 export default App;
