262d20cdbaefd403356e9b972f734c55bee8685a
blakebdavies <blakebdavies@gmail.com>
2022-09-12 20:05:28 +0100

    Created necessary component files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import {BrowswerRouter, Routes, Route} from 'react-router-dom';
 import {Box} from '@mui/material';
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+
+
+
 
 const App = () => (
     <BrowswerRouter>
         <Box sx={{backgroundColor:'#000'}}>
-            <NavBar />
+            <Navbar />
             <Routes>
                 <Route path="/" exact element={<Feed />} />
-                <Route path="/video/:id" element={<VideoDetail/>}/>
-                <Route path="/video/:id" element={<ChannelDetail/>}/>
-                <Route path="/video/:searchTerm" element={<SearchFeed/>}/>
+                <Route path="/video/:id" element={<VideoDetail />}/>
+                <Route path="/channel/:id" element={<ChannelDetail />}/>
+                <Route path="/search/:searchTerm" element={<SearchFeed />}/>
             </Routes>
         </Box>
     </BrowswerRouter>
