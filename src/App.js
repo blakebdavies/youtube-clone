73df3a8b4df11e8e80dc072f2d59986954cb0c63
blakebdavies <blakebdavies@gmail.com>
2022-09-12 17:25:05 +0100

    Set up project routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import {BrowswerRouter, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
 
-const App = () => {
-    return (
-        <div>App</div>
+const App = () => (
+    <BrowswerRouter>
+        <Box sx={{backgroundColor:'#000'}}>
+            <NavBar />
+            <Routes>
+                <Route path="/" exact element={<Feed />} />
+                <Route path="/video/:id" element={<VideoDetail/>}/>
+                <Route path="/video/:id" element={<ChannelDetail/>}/>
+                <Route path="/video/:searchTerm" element={<SearchFeed/>}/>
+            </Routes>
+        </Box>
+    </BrowswerRouter>
     );
-};
 
 export default App;
