3e7beff620aa204fb5900fde80502b485939f080
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-09-13 14:10:44 +0100

    Working on Feed component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import {Stack} from '@mui/material';
 
 import {categories} from '../utils/constants';
 
-const selectedCategory ='New';
+const selectedCategory = 'new';
 
 const Sidebar = () => {
     return (
@@ -10,8 +10,8 @@ const Sidebar = () => {
             direction="row"
             sx={{
                 overflowY: "auto",
-                height: { sx: 'auto', md: '95%' },
-                flexDirection: {md: 'column' },
+                height: {sx: 'auto', md: '95%'},
+                flexDirection: {md: 'column'},
             }}
         >
             {categories.map((category) => (
@@ -23,8 +23,11 @@ const Sidebar = () => {
                     }}
                     key={category.name}
                 >
-                    <span style={{color: category.name === selectedCategory ? '#fff' : '#FC1503', marginRight: '15px'}}>{category.icon}</span>
-                    <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+                    <span style={{
+                        color: category.name === selectedCategory ? '#fff' : '#FC1503',
+                        marginRight: '15px'
+                    }}>{category.icon}</span>
+                    <span style={{opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
                 </button>
             ))}
         </Stack>
