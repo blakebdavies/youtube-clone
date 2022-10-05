1f0850275af5fbd14480c0f7cabbeaff195ced1a
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-05 16:11:25 +0100

    updated files

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,7 +12,6 @@ const Feed = () => {
         fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
             .then((data) => setVideos(data.items))
     }, [selectedCategory]);
-
     return (
         <Stack sx={{flexDirection: {sx: "column", md: "row"}}}>
             <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
