97833f2a19669afa604f11d2b6bbd846fb7cffa1
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-11 21:39:07 +0100

    Created and styled Video Details component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {Paper, IconButton} from '@mui/material';
-import {Search} from '@mui/icons-material';
 import SearchIcon from '@mui/icons-material/Search';
 
 
