49619f53a8810fae953f4d8808f091ef0e692462
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-11 22:53:53 +0100

    fixed build error in Video Card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -9,7 +9,7 @@ const VideoCard = ({video: {id: {videoId}, snippet}}) => {
         <Card sx={{width: {sx: '100%', sm: '358px', md: '320px'}, boxShadow: 'none', borderRadius: 0}}>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
                 <CardMedia
-                    image={snippet?.thumbnails?.high?.url}
+                    image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
                     alt={snippet?.title}
                     sx={{width: {sx: '100%', sm: '358px'}, height: 180}}/>
             </Link>
